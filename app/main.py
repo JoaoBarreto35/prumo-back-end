72ce2cf2f626c1e56b26cd6f8a1841858561ccd3
JoaoBarreto35 <143046404+JoaoBarreto35@users.noreply.github.com>
2026-06-29 10:54:03 -0300

    fix(sad)

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ def create_application() -> FastAPI:
 
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=[settings.allowed_orings],
+        allow_origins=[settings.allowed_origins],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
